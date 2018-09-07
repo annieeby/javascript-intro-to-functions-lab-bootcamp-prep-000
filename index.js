d399c6fc38093b354ee76f608b350bc09a056927
Annie Eby <eby.annie@gmail.com>
2018-09-07 10:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 
 function logShout(string){
-  console.log(shout)
+  console.log(shout(string))
 }
 
 //function logShout(string) {console.log(string.toUpperCase())}
