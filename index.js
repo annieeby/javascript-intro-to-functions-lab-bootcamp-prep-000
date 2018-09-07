dbe475d474e0c7871aa2c41725631c83005b009f
Annie Eby <eby.annie@gmail.com>
2018-09-07 10:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function shout(string) {return string.toUpperCase()}
 
 function whisper(string) {return string.toLowerCase()}
 
-function logShout(string) {console.log(string.toUpperCase())}
+function logShout(string){
+  console.log(shout)
+}
+
+//function logShout(string) {console.log(string.toUpperCase())}
 
 function logWhisper(string) {console.log(string.toLowerCase())}
 
